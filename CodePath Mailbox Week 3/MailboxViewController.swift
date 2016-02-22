176813f0de1435815e403e22cee7fe9611093ecb
Jess <jesswlam@gmail.com>
2016-02-21 17:06:59 -0800

    Reset message and scroll view.

--- CodePath Mailbox Week 3/MailboxViewController.swift
+++ CodePath Mailbox Week 3/MailboxViewController.swift
@@ -15,6 +15,9 @@ class MailboxViewController: UIViewController {
     @IBOutlet weak var singleMessageView: UIImageView!
     @IBOutlet weak var messageBackgroundView: UIView!
     @IBOutlet weak var feedWrapperView: UIView!
+    @IBOutlet weak var mainWrapperView: UIView!
+    @IBOutlet weak var menuView: UIImageView!
+    @IBOutlet weak var feedImageView: UIImageView!
     
     @IBOutlet weak var rightIconView: UIView!
     @IBOutlet weak var leftIconView: UIView!
@@ -42,6 +45,21 @@ class MailboxViewController: UIViewController {
     var singleMessageArchived: Bool!
     var singleMessageDeleted: Bool!
     var singleMessageMoved: Bool!
+    var mainWrapperSwiped: Bool!
+    
+    //main wrapper view variables
+    var mainWrapperInitialCenter: CGPoint!
+    var mainWrapperOriginalPosition: CGFloat!
+    var mainWrapperRightPosition: CGFloat!
+    var mainWrapperStartRightPositionX: CGFloat!
+    var mainWrapperStartRightPosition: CGPoint!
+    
+    //variables for colors
+    var lightGrey: UIColor!
+    var green: UIColor!
+    var red: UIColor!
+    var yellow: UIColor!
+    var brown: UIColor!
     
     
     override func viewDidLoad() {
@@ -63,10 +81,28 @@ class MailboxViewController: UIViewController {
         singleMessageArchived = false
         singleMessageDeleted = false
         singleMessageMoved = false
+        mainWrapperSwiped = false
         
         //set feed wrapper view values
         feedWrapperViewInitialY = feedWrapperView.frame.origin.y
-        feedWrapperViewOffset = -86
+        feedWrapperViewOffset = -88
+        
+        //initiate pan edge recognizer
+        let edgeGesture = UIScreenEdgePanGestureRecognizer(target: self, action: "didPanEdgeMainView:")
+        mainWrapperView.addGestureRecognizer(edgeGesture)
+        
+        //set menu view positions
+        mainWrapperOriginalPosition = mainWrapperView.center.x
+        mainWrapperRightPosition = mainWrapperView.center.x + 280
+        mainWrapperStartRightPositionX = mainWrapperView.center.x + 280
+        
+        //set color values
+        lightGrey = UIColor(red: 229/255, green: 230/255, blue: 233/255, alpha: 1.0)
+        green = UIColor(red: 98/255, green: 214/255, blue: 80/255, alpha: 1.0)
+        red = UIColor(red: 229/255, green: 62/255, blue: 39/255, alpha: 1.0)
+        yellow = UIColor(red: 233/255, green: 189/255, blue: 38/255, alpha: 1.0)
+        brown = UIColor(red: 207/255, green: 150/255, blue: 99/255, alpha: 1.0)
+        
 
         // Do any additional setup after loading the view.
     }
@@ -107,7 +143,7 @@ class MailboxViewController: UIViewController {
             //pan right less than 60, gray bg, move back to center
             if translation.x >= 0 && translation.x < 60 {
                 print("translation.x > 0")
-                messageBackgroundView.backgroundColor = UIColor.grayColor()
+                messageBackgroundView.backgroundColor = lightGrey
                 listIconView.hidden = true
                 laterIconView.hidden = true
                 archiveIconView.hidden = false
@@ -117,7 +153,7 @@ class MailboxViewController: UIViewController {
             //pan to right more than 60, green bg, move offscreen
             else if translation.x >= 60 && translation.x < 260 {
                 print("translation.x > 60")
-                messageBackgroundView.backgroundColor = UIColor.greenColor()
+                messageBackgroundView.backgroundColor = green
                 leftIconView.transform = CGAffineTransformMakeTranslation(leftIconViewConvertedTranslation, 0)
                 listIconView.hidden = true
                 laterIconView.hidden = true
@@ -128,8 +164,7 @@ class MailboxViewController: UIViewController {
             //pan to right more than 260, red bg, move offscreen
             else if translation.x > 260 {
                 print("translation.x > 260")
-                messageBackgroundView.backgroundColor = UIColor.redColor()
-                messageBackgroundView.backgroundColor = UIColor.greenColor()
+                messageBackgroundView.backgroundColor = red
                 leftIconView.transform = CGAffineTransformMakeTranslation(leftIconViewConvertedTranslation, 0)
                 listIconView.hidden = true
                 laterIconView.hidden = true
@@ -141,7 +176,7 @@ class MailboxViewController: UIViewController {
             //pan left less than 60 and see gray bg and later icon
             else if translation.x <= 0 && translation.x > -60 {
                 print("translation.x < 0")
-                messageBackgroundView.backgroundColor = UIColor.grayColor()
+                messageBackgroundView.backgroundColor = lightGrey
                 listIconView.hidden = true
                 laterIconView.hidden = false
                 archiveIconView.hidden = true
@@ -151,7 +186,7 @@ class MailboxViewController: UIViewController {
             //pan left to show later icon and yello bg
             else if translation.x <= -60 && translation.x > -260 {
                 print("translation.x < -60")
-                messageBackgroundView.backgroundColor = UIColor.yellowColor()
+                messageBackgroundView.backgroundColor = yellow
                 rightIconView.transform = CGAffineTransformMakeTranslation(rightIconViewConvertedTranslation, 0)
                 listIconView.hidden = true
                 laterIconView.hidden = false
@@ -162,7 +197,7 @@ class MailboxViewController: UIViewController {
             //pan left more than 260 to show list icon and brown bg color
             else if translation.x <= -260 {
                 print("translation.x < -260")
-                messageBackgroundView.backgroundColor = UIColor.brownColor()
+                messageBackgroundView.backgroundColor = brown
                 rightIconView.transform = CGAffineTransformMakeTranslation(rightIconViewConvertedTranslation, 0)
                 listIconView.hidden = false
                 laterIconView.hidden = true
@@ -182,13 +217,13 @@ class MailboxViewController: UIViewController {
                     self.singleMessageView.center.x = self.rightPosition
                     self.singleMessageArchived = true
                     
-//                    UIView.animateWithDuration(0.1, animations: { () -> Void in
-//                        self.archiveIconView.alpha = 0
-//                        }, completion: { (Bool) -> Void in
-//                            UIView.animateWithDuration(0.2, animations: { () -> Void in
-//                                self.feedWrpperView.frame.origin.y = self.feedWrapperViewInitialY + self.feedWrapperViewOffset
-//                            })
-//                    })
+                    UIView.animateWithDuration(0.1, animations: { () -> Void in
+                        self.archiveIconView.alpha = 0
+                        }, completion: { (Bool) -> Void in
+                            UIView.animateWithDuration(0.2, animations: { () -> Void in
+                                self.feedWrapperView.frame.origin.y = self.feedWrapperViewInitialY + self.feedWrapperViewOffset
+                            })
+                    })
                 }
                 
                 //pan to right and delete message
@@ -197,7 +232,13 @@ class MailboxViewController: UIViewController {
                     self.singleMessageView.center.x = self.rightPosition
                     self.singleMessageDeleted = true
 
-                    //might need feed wrapper animation
+                    UIView.animateWithDuration(0.1, animations: { () -> Void in
+                        self.deleteIconView.alpha = 0
+                        }, completion: { (Bool) -> Void in
+                            UIView.animateWithDuration(0.2, animations: { () -> Void in
+                                self.feedWrapperView.frame.origin.y = self.feedWrapperViewInitialY + self.feedWrapperViewOffset
+                            })
+                    })
                 }
                 
                 //pan left and show later menu
@@ -241,7 +282,7 @@ class MailboxViewController: UIViewController {
         
     }
 
-    
+    //dismissing the reschedule menu
     @IBAction func didTapRescheduleView(sender: UITapGestureRecognizer) {
         UIView.animateWithDuration(0.2, animations: { () -> Void in
             self.rescheduleView.alpha = 0
@@ -249,6 +290,67 @@ class MailboxViewController: UIViewController {
                 UIView.animateWithDuration(0.2, animations: { () -> Void in
                     self.feedWrapperView.frame.origin.y = self.feedWrapperViewInitialY + self.feedWrapperViewOffset
                 })
+        }
+    }
+    
+    //dismiss list menu
+    @IBAction func didTapListView(sender: UITapGestureRecognizer) {
+        UIView.animateWithDuration(0.2, animations: { () -> Void in
+            self.listView.alpha = 0
+            }) { (Bool) -> Void in
+                UIView.animateWithDuration(0.2, animations: { () -> Void in
+                    self.feedWrapperView.frame.origin.y = self.feedWrapperViewInitialY + self.feedWrapperViewOffset
+                })
+        }
+        
+    }
+    
+    //reset mailbox
+    @IBAction func tapResetMailbox(sender: UITapGestureRecognizer) {
+        singleMessageView.frame.origin.x = 0
+        UIView.animateWithDuration(0.3) { () -> Void in
+            self.messageBackgroundView.hidden = false
+            self.feedImageView.center.y += 88
+            self.feedScrollView.contentSize.height += 88
+            self.listIconView.hidden = false
+            self.laterIconView.hidden = false
+            self.archiveIconView.hidden = false
+            self.deleteIconView.hidden = false
+        }
+    }
+    
+    
+    //edge pan the main view. not working at the moment.
+    @IBAction func didPanEdgeMainView(sender: UIScreenEdgePanGestureRecognizer) {
+        let translation = sender.translationInView(view)
+        let velocity = sender.velocityInView(view)
+        print("Edge panning + \(translation.x)")
+        print(velocity.x)
+        
+        if sender.state == UIGestureRecognizerState.Began {
+            print("edge pan began")
+            mainWrapperInitialCenter = mainWrapperView.center
+        }
+        
+        //move main wrapper center
+        else if sender.state == UIGestureRecognizerState.Changed {
+            print("edge pan changed")
+            mainWrapperView.center = CGPoint(x: translation.x + mainWrapperInitialCenter.x, y: mainWrapperInitialCenter.y)
+        }
+        
+        else if sender.state == UIGestureRecognizerState.Ended {
+            print("edge pan ended")
+            UIView.animateWithDuration(0.3, animations: { () -> Void in
+                if velocity.x > 0 {
+                    self.mainWrapperView.center.x = self.mainWrapperRightPosition
+                    self.mainWrapperSwiped = true
+                }
+                
+                else if velocity.x < 0 {
+                    self.mainWrapperView.center.x = self.mainWrapperOriginalPosition
+                    self.mainWrapperSwiped = false
+                }
+            })
         }
     }
     
